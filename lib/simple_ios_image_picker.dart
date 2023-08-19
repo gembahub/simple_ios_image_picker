39c14f549a20829d4c59b2054cdeac68c63eca80
Masahiro Aoki <freud427@gmail.com>
2023-08-19 23:26:04 +0900

    update

--- lib/simple_ios_image_picker.dart
+++ lib/simple_ios_image_picker.dart
@@ -1,9 +1,10 @@
+import 'package:cross_file/cross_file.dart';
 import 'package:flutter/services.dart';
 
 class SimpleIosImagePicker {
   final methodChannel = const MethodChannel('simple_ios_image_picker');
 
-  Future<List<Uint8List>?> pickImages({
+  Future<List<XFile>?> pickImages({
     int limit = 10,
     double compressionQuality = 1.0,
   }) async {
@@ -12,13 +13,39 @@ class SimpleIosImagePicker {
       "limit": limit,
       'compressionQuality': compressionQuality,
     });
-    if (resultList != null) {
-      return resultList.map((item) => item as Uint8List).toList();
+
+    if (resultList == null) {
+      return null;
     }
-    return null;
+    final uint8ListList = _nativeResultToUint8ListList(resultList);
+    final xFileList = await _uint8ListListToXFileList(uint8ListList);
+    return xFileList;
   }
 
-  Future<void> _uint8ListToXFile() async {
-    //
+  /// convert native result to Uint8ListList
+  List<Uint8List> _nativeResultToUint8ListList(List<dynamic> nativeResult) {
+    var uint8ListList = <Uint8List>[];
+
+    for (var item in nativeResult) {
+      uint8ListList.add(item as Uint8List);
+    }
+
+    return uint8ListList;
+  }
+
+  /// convert Uint8List to XFile
+  Future<XFile> _uint8ListToXFile(Uint8List bytes) async =>
+      XFile.fromData(bytes);
+
+  /// convert multiple Uint8List to XFile
+  Future<List<XFile>> _uint8ListListToXFileList(
+      List<Uint8List> uint8ListList) async {
+    var xFileList = <XFile>[];
+
+    for (var uint8List in uint8ListList) {
+      xFileList.add(await _uint8ListToXFile(uint8List));
+    }
+
+    return xFileList;
   }
 }
